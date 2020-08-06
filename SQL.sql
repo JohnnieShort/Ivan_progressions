67d5e6b5b89df3396e57545ff2da4446e5bceeeb
DESKTOP-7M236TH\johnn <johnnie.short@gmail.com>
2020-08-06 19:26:50 +0300

    added data model diagram and SQL script to generate DB tables

---
+++ SQL.sql
@@ -0,0 +1,43 @@
+CREATE TABLE "Album" (
+  "id" int4 NOT NULL,
+  "name" varchar(255),
+  "fk_artist_id" int4 NOT NULL,
+  "release_date" date,
+  "fk_label_id" int4 NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Artist" (
+  "id" int4 NOT NULL,
+  "name" varchar(255),
+  "country" varchar(255),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Label" (
+  "id" int4 NOT NULL,
+  "name" varchar(255),
+  "country" varchar(255),
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "Track" (
+  "id" int4 NOT NULL,
+  "duration" int4 NOT NULL,
+  "name" varchar(255) NOT NULL,
+  "fk_album_id" int4 NOT NULL,
+  PRIMARY KEY ("id")
+);
+
+CREATE TABLE "User" (
+  "id" int4 NOT NULL,
+  "nickname" varchar(255),
+  "login" varchar(255),
+  "password" varchar(255),
+  PRIMARY KEY ("id")
+);
+
+ALTER TABLE "Album" ADD CONSTRAINT "fk_artist_id" FOREIGN KEY ("fk_artist_id") REFERENCES "Artist" ("id");
+ALTER TABLE "Album" ADD CONSTRAINT "fk_label_id" FOREIGN KEY ("fk_label_id") REFERENCES "Label" ("id");
+ALTER TABLE "Track" ADD CONSTRAINT "fk_album_id" FOREIGN KEY ("fk_album_id") REFERENCES "Album" ("id");
+
